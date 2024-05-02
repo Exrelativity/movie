66b49c545253fa436518335fcc945646cd26d51a
relativity-codes <ukweheverest@gmail.com>
2024-05-02 15:44:13 +0100

    update

--- src/MovieDetails.tsx
+++ src/MovieDetails.tsx
@@ -5,9 +5,10 @@ import React from "react";
 const MovieDetails = ({ movie }: { movie: any }): React.ReactNode => {
   return (
     <div className="container flex flex-col items-start justify-start">
-      <h3 className='w-full py-4 text-center text-xl font-bold'><span className="font-bold">Title:</span> {movie.Title}</h3>
+      <h3 className='w-full py-4 text-center text-xl font-bold'>{movie.Title}</h3>
       <div className='flex flex-row justify-start'><div className='flex flex-row justify-start'><img className='w-[600px] rounded-md' src={movie.Poster} alt={movie.Title} /></div>
-        <div className='flex flex-col justify-start px-3'>
+        <div className='flex flex-col items-start justify-start gap-1 px-3'>
+          <p><span className="font-bold">Title:</span> {movie.Title}</p>
           <p><span className="font-bold">Year:</span> {movie.Year}</p>
           <p><span className="font-bold">Genre:</span> {movie.Genre}</p>
           <p><span className="font-bold">IMDB Rating:</span> {movie.imdbRating}</p>
