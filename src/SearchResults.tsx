2d7b05288f720aae6e059cd4726e02e5fad6c2ca
relativity-codes <ukweheverest@gmail.com>
2024-05-02 15:39:35 +0100

    update

--- src/SearchResults.tsx
+++ src/SearchResults.tsx
@@ -2,17 +2,20 @@
 
 import React from "react";
 
-const SearchResults = ({ results, onSelectMovie }:{[key: string]:any}):React.ReactNode => {
+const SearchResults = ({ results, onSelectMovie }: { [key: string]: any }): React.ReactNode => {
   return (
-    <div className="flex flex-col container justify-start items-start">
-      <h3 className='w-full text-center text-xl font-bold py-4'>Search Results</h3>
-      <ul className='list-decimal list-outside'>
-        {results.map((movie:any) => (
-          <li className='hover:text-blue-950 cursor-pointer hover:underline' key={movie.imdbID} onClick={() => onSelectMovie(movie.imdbID)}>
-            {movie.Title}
-          </li>
+    <div className="container flex flex-col items-start justify-start">
+      <h3 className='w-full py-4 text-center text-xl font-bold'>Search Results</h3>
+      <div className='flex w-full flex-row items-center justify-start gap-3 overflow-auto'>
+        {results.map((movie: any) => (
+          <div className='flex w-80 cursor-pointer flex-col items-start justify-start py-2 hover:text-blue-950 hover:underline' key={movie.imdbID} onClick={() => onSelectMovie(movie.imdbID)}>
+            <div className="flex w-full flex-row items-center justify-center">
+              <img src={movie.Poster} className="aspect-square w-80 min-w-80 rounded-md" alt="movie poster" />
+            </div>
+            <div className="w-full truncate p-1 font-bold">{movie.Title}</div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
